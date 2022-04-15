273c6cd1fbc9884144685d101a5f7cdf5e0e9276
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-04-15 15:18:03 -0500

    removed unneeded style element from __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -60,15 +60,3 @@
 <AutoAdjust {topAppBar}>
   <slot />
 </AutoAdjust>
-
-<style>
-  /* Hide everything above this component. */
-  :global(app),
-  :global(body),
-  :global(html) {
-    display: block !important;
-    height: auto !important;
-    width: auto !important;
-    position: static !important;
-  }
-</style>
