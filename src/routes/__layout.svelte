c24b50c5194b284f55b369c4d043baa0aaaae14d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-04-15 16:22:55 -0500

    drawer progress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   import {onMount} from 'svelte';
+  //import Button, {Label} from '@smui/button';
+  import {H6} from '@smui/common/elements';
+  import Drawer, {AppContent, Content, Header, Subtitle} from '@smui/drawer';
+  import List, {Graphic, Item, Separator, Subheader, Text} from '@smui/list';
+  import TopAppBar, {AutoAdjust, Row, Section, Title} from '@smui/top-app-bar';
   import type {TopAppBarComponentDev} from '@smui/top-app-bar';
-  import TopAppBar, {Row, Section, Title, AutoAdjust} from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
 
+  let activePage = 'Inbox';
   let darkTheme: boolean;
+  let drawerOpen = false;
   let topAppBar: TopAppBarComponentDev;
 
   $: modeLabel = `switch to ${darkTheme ? 'light' : 'dark'} mode`;
@@ -16,6 +22,13 @@
     darkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
   });
 
+  function setActive(page: string) {
+    console.log('__layout.svelte setActive: page =', page);
+    activePage = page;
+    drawerOpen = false;
+  }
+
+  const toggleDrawer = () => (drawerOpen = !drawerOpen);
   const toggleMode = () => (darkTheme = !darkTheme);
 </script>
 
@@ -42,8 +55,10 @@
 <TopAppBar bind:this={topAppBar} variant="standard">
   <Row>
     <Section>
-      <IconButton class="material-icons">menu</IconButton>
-      <Title>Standard</Title>
+      <IconButton class="material-icons" on:click={toggleDrawer}>
+        menu
+      </IconButton>
+      <Title>{activePage}</Title>
     </Section>
     <Section align="end" toolbar>
       <IconButton
@@ -57,6 +72,109 @@
     </Section>
   </Row>
 </TopAppBar>
-<AutoAdjust {topAppBar}>
-  <slot />
-</AutoAdjust>
+
+<div class="drawer-container">
+  <Drawer variant="modal" fixed={false} bind:open={drawerOpen}>
+    <Header>
+      <Title>Super Mail</Title>
+      <Subtitle>It's the best fake mail app drawer.</Subtitle>
+    </Header>
+    <Content>
+      <List>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Inbox')}
+          activated={activePage === 'Inbox'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">inbox</Graphic>
+          <Text>Inbox</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Star')}
+          activated={activePage === 'Star'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">star</Graphic>
+          <Text>Star</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Sent Mail')}
+          activated={activePage === 'Sent Mail'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">send</Graphic>
+          <Text>Sent Mail</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Drafts')}
+          activated={activePage === 'Drafts'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">drafts</Graphic>
+          <Text>Drafts</Text>
+        </Item>
+
+        <Separator />
+        <Subheader component={H6}>Labels</Subheader>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Family')}
+          activated={activePage === 'Family'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Family</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Friends')}
+          activated={activePage === 'Friends'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Friends</Text>
+        </Item>
+        <Item
+          href="javascript:void(0)"
+          on:click={() => setActive('Work')}
+          activated={activePage === 'Work'}
+        >
+          <Graphic class="material-icons" aria-hidden="true">bookmark</Graphic>
+          <Text>Work</Text>
+        </Item>
+      </List>
+    </Content>
+  </Drawer>
+
+  <div class="main-content">
+    <AutoAdjust {topAppBar}>
+      <slot />
+    </AutoAdjust>
+  </div>
+</div>
+
+<style>
+  .drawer-container {
+    position: relative;
+    display: flex;
+    /* height: 350px; */
+    height: 100%;
+    max-width: 600px;
+    border: 1px solid
+      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    overflow: hidden;
+    z-index: 0;
+  }
+
+  .main-content {
+    overflow: auto;
+    padding: 16px;
+    height: 100%;
+    box-sizing: border-box;
+  }
+
+  * :global(.app-content) {
+    flex: auto;
+    overflow: auto;
+    position: relative;
+    flex-grow: 1;
+  }
+</style>
